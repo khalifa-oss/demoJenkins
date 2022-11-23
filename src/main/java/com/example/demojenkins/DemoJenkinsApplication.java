22c2111a64cc7c574ca0bdad823e5cc4285030f8
khalifa-oss <saidikhalifa22@yahoo.fr>
2022-11-23 09:36:11 +0100

    first commit

---
+++ src/main/java/com/example/demojenkins/DemoJenkinsApplication.java
@@ -0,0 +1,24 @@
+package com.example.demojenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+ class Test{
+    @GetMapping("/hello")
+    public String display(){
+        return "hello word ";
+
+    }
+}
+
+@SpringBootApplication
+public class DemoJenkinsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run( DemoJenkinsApplication.class, args );
+    }
+
+}
